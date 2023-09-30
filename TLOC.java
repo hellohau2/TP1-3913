14401e76008185f729bd816ef6c4505e28b4e022
hellohau <143982708+hellohau2@users.noreply.github.com>
2023-09-30 14:37:34 -0400

    Completion TLOC
    La classe TLOC est terminée.
    On peut tester avec le fichier TLOC_tesfile.java.
    
    Dans le terminal :
    java TLOC.java TLOC_testfile.java
    
    qui devrait retourner :
    TLOC : 9
    
    Il ne manque plus qu'à compiler le fichier java en exécutable -jar pour la remise.

--- TLOC.java
+++ TLOC.java
@@ -12,16 +12,33 @@ public class TLOC{
         System.out.println("TLOC : " + countTLOC(args[0]));
     }
 
+    // Argument : String file = chemin du fichier pour TLOC 
+    // Output : int tloc = Le nombre de ligne qui ne sont pas vide et ne sont pas des commentaires
     static int countTLOC(String file){
         int tloc = 0;
         try(BufferedReader br = new BufferedReader(new FileReader(file))){
-            String text = "";
+            Boolean commentStart = false;
             String line;
             while ((line = br.readLine()) != null){
-                text += line;
+                // Enleve les espaces 
+                line = line.trim();
+                // Check si on commence un commentaire
+                if (line.startsWith("/*")){
+                    commentStart = true;
+                }
+
+                // Check si c'est une ligne de code
+                if (!commentStart && !line.startsWith("//") && !line.isEmpty()){
+                    tloc++;
+                }
+
+                // Check si on n'est plus dans un commentaire
+                if (commentStart && line.endsWith("*/")){
+                    commentStart = false;
+                }
             }
 
-            System.out.println(text);
+
         }catch(IOException e){
             e.printStackTrace();
         }
