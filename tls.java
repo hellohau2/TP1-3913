9098e10673e7369ac4d0964f36fe0a729e637314
vyncerose <vyncent.l.larose@gmail.com>
2023-10-03 12:34:43 -0400

    Gestion des fichiers

--- tls.java
+++ tls.java
@@ -1,17 +1,27 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class tls {
 
     public static void main(String[] args) {
+        String folderPath = "";
+        Boolean createCsv = false;
         if (args.length == 0) {
+            folderPath = "./";
             // tls
             // Faire tls a l'interieur du dossier actuel
         }
         else if (args.length == 1) {
-            String folderPath = args[0];
+            folderPath = args[0];
             // tls path
             // Sortie en ligne de commande, pas de fichier .csv produit
         }
         else if (args.length == 3 && args[0] == "-o") {
-            String folderPath = args[2];
+            folderPath = args[2];
+            createCsv = true;
             // tls -o <chemin-à-la-sortie.csv> <chemin-de-l'entrée>
             // Sortie dans un fichier .csv
         }
@@ -19,9 +29,31 @@ public class tls {
             System.out.println("Fichier inexistant");
             System.exit(1);
         }
+
+        File dossier = new File(folderPath);
+        FilenameFilter filter = (dir, nomFichier) -> nomFichier.endsWith(".java");
+        File[] fichiersJava = dossier.listFiles(filter);
+
+        if (fichiersJava != null) {
+            if (createCsv == true) {
+                outputCsv();
+            }
+            else {
+                outputTerminal();
+            }
+        }
+        else {
+            System.out.println("Ce dossier ne contient aucun fichier Java.");
+        }
     }
 
-    public void allJavaFiles(String folderPath) {
-        // TODO: retourne ou cree une liste contenant tous les noms de fichiers Java
+    public static void outputTerminal() {
+        String x = "chemin du fichier, nom du paquet, nom de la classe, tloc de la classe, tassert de la classe, tcmp de la classe\n";
+        // Do everything else
+        System.out.println(x);
+    }
+
+    public static void outputCsv() {
+
     }
 }
